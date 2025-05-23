0a4e37ece989480b15ac6ae8a3df1a6542315dfa
Leonardo-de-Moura <francisco.tico.fssm@gmail.com>
2025-05-23 11:25:37 -0300

    up

--- atividade.py
+++ atividade.py
@@ -1,16 +1,37 @@
 dados = []
+dadostransporte= []
 
 while True:
+	ask= input("quer adicionar? [S/N]")
+	if ask=="S":
+		nome= input("digite seu nome")
+		idade= input("digite sua idade")
+		carro= input("digite seu carro")
+
+		pessoa= { 
+				"id": len(dados) + 1,
+				"nome": nome,
+				"idade": idade,
+				"endereco": carro}
+		dados.append(pessoa)
+		print("pessoa adicionada a lista")
+		print("lista atual:", dados)
+		print()
+	else: 
+		break
+
+ask= input("tem transporte?[S/N]")
+if ask== "N":
 	nome= input("digite seu nome")
 	idade= input("digite sua idade")
-	carro= input("digite seu carro")
-
 	pessoa= { 
-			"id": len(dados) + 1,
-			"nome": nome,
-			"idade": idade,
-			"endereco": carro}
-	dados.append(pessoa)
+	"id": len(dadostransporte) + 1,
+		"nome": nome,
+		"idade": idade,
+		"endereco": carro}
+	dadostransporte.append(pessoa)
 	print("pessoa adicionada a lista")
-	print("lista atual:", dados)
+	print("lista atual:", dadostransporte)
 	print()
+else:
+	print("ja que tem transporte, nao e necessario adicona-lo a lista")
